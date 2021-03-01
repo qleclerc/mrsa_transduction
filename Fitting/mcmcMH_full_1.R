fc1ad27b09b99081a085846e36855b0180d3b4c7
qleclerc <quentin.leclerc@lshtm.ac.uk>
2021-03-01 23:05:48 +0100

    reset mcmc 10 3

--- Fitting/mcmcMH_full_1.R
+++ Fitting/mcmcMH_full_1.R
@@ -111,7 +111,7 @@ for(i in 1:nrow(models_to_try)){
   #                      link_delay = F,
   #                      transduction = T)
   init.theta = c(beta = 1e10, L = 80, gamma = 30000, alpha = 1e6, tau = 0.3)
-  mcmc_fit = run_mcmc(model, lab_data_trans5,
+  mcmc_fit = run_mcmc(model, lab_data_trans3,
                       init.theta = init.theta,
                       proposal.sd = c(init.theta[1]/1000,
                                       init.theta[2]/1000,
@@ -277,12 +277,12 @@ for(i in 1:nrow(models_to_try)){
             legend)
   
   filename = paste0(models_to_try$model_name[i], ".png")
-  ggsave(here::here("Fitting", "10_5", "Best_fits", filename))
+  ggsave(here::here("Fitting", "10_3", "Best_fits", filename))
   
   all_theta[[i]] = mcmc_fit$trace
   names(all_theta)[i] = models_to_try$model_name[i]
   
 }
 
-saveRDS(all_theta, here::here("Fitting", "10_5", "best_params_transduction.rds"))
+saveRDS(all_theta, here::here("Fitting", "10_3", "best_params_transduction.rds"))
 
