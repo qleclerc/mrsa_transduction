b9d425d797063e7464699b0fa37d63a4bd555f37
qleclerc <quentin.leclerc@lshtm.ac.uk>
2021-05-27 22:35:58 +0100

    reset mcmc

--- Fitting/mcmcMH_full_1.R
+++ Fitting/mcmcMH_full_1.R
@@ -86,11 +86,11 @@ for(i in 1:nrow(models_to_try)){
                        link_delay = models_to_try$link_delay[i],
                        transduction = models_to_try$transduction[i])
   
-  # trace_model4 = fitted_params4[[models_to_try$model_name[i]]]
-  # init.theta = trace_model4[nrow(trace_model4),-5]
+  trace_model4 = fitted_params4[[models_to_try$model_name[i]]]
+  init.theta = trace_model4[nrow(trace_model4),-5]
   
   
-  init.theta = c(beta = 5e9, L = 60, alpha = 1e6, tau = 0.6)
+  # init.theta = c(beta = 5e9, L = 60, alpha = 1e6, tau = 0.6)
   mcmc_fit = run_mcmc(model, lab_data_trans3, lab_data_trans5,
                       init.theta = init.theta,
                       proposal.sd = c(init.theta[1]/300,
@@ -99,7 +99,7 @@ for(i in 1:nrow(models_to_try)){
                                       init.theta[4]/300),
                       n.iterations = 100000,
                       adapt.size.start = 1000,
-                      adapt.shape.start = 4000,
+                      adapt.shape.start = NULL,
                       adapt.size.cooling = 0.999)
   
   
@@ -235,8 +235,8 @@ for(i in 1:nrow(models_to_try)){
   filename = paste0(models_to_try$model_name[i], ".png")
   ggsave(here::here("Fitting", "Full_chains", "Best_fits", filename))
   
-  # all_theta[[i]] = rbind(trace_model4, mcmc_fit$trace)
-  all_theta[[i]] = mcmc_fit$trace
+  all_theta[[i]] = rbind(trace_model4, mcmc_fit$trace)
+  # all_theta[[i]] = mcmc_fit$trace
   names(all_theta)[i] = models_to_try$model_name[i]
   
 }
