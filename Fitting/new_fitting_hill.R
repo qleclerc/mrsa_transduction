267fa70281ac3e30f2fd73ff25795bc970dea6a0
Quentin Leclerc <39052287+qleclerc@users.noreply.github.com>
2022-07-29 21:03:34 +0100

    Update new_fitting_hill.R

--- Fitting/new_fitting_hill.R
+++ Fitting/new_fitting_hill.R
@@ -192,7 +192,7 @@ phage_tr_model = function(parameters,
 # load reference parameter definition (upper, lower prior)
 refPars <- data.frame(best = c(3, 60, 2, 0.67, 1, 70),
                       lower = c(1, 10, 0.1, 0.6, 0.9, 1),
-                      upper = c(10, 90, 6, 0.8, 1.5, 100))
+                      upper = c(10, 90, 3, 0.8, 1.5, 100))
 rownames(refPars) = c("beta", "L", "alpha", "tau", "pow", "P_lim")
 
 parSel = c(1:4,6)
@@ -229,10 +229,10 @@ likelihood <- function(par, mode = "hill"){
                     predicted105$Bet,
                     log = T)
   
-  llValues5 = 4*dpois(x = round(obs105$Be[12:18]/(10^(pmax(floor(log10(obs105$Be[12:18])),1)-1))),
+  llValues5 = 2*dpois(x = round(obs105$Be[12:18]/(10^(pmax(floor(log10(obs105$Be[12:18])),1)-1))),
                     lambda = predicted105$Be[12:18]/(10^(pmax(floor(log10(obs105$Be[12:18])),1)-1)),
                     log = T)
-  llValues6 = 4*dpois(x = round(obs105$Bt[12:18]/(10^(pmax(floor(log10(obs105$Bt[12:18])),1)-1))),
+  llValues6 = 2*dpois(x = round(obs105$Bt[12:18]/(10^(pmax(floor(log10(obs105$Bt[12:18])),1)-1))),
                     lambda = predicted105$Bt[12:18]/(10^(pmax(floor(log10(obs105$Bt[12:18])),1)-1)),
                     log = T)
   
